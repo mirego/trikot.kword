b77c8f46e7890ce25925ea240a25af3526a62a9c
Nicolas Presseault <npresseault@mirego.com>
2020-04-27 13:50:21 -0400

    - Add support for multiple sources of strings.  For instance it is now possible to have something like:  - translations.en.json  - translations.en.brand1.json  - translations.en.brand2.json  - translations.fr.json  - translations.fr.brand1.json  - translations.fr.brand2.json
    - Placeholder resolution now lookup into translated string table as well.
     For instance
    "foo_key": "foo"
    "bar_key": "bar"
    "foo_bar_key": "this is {{foo_key}} {{bar_key}}"
    
    KWord[FOO_BAR_KEY] will resolve to "this is foo bar"
    
    - kword now inherit from DefaultI18N where all the logic was moved.
    
    Everything is backward compatible and applications using KWord will have nothing to migrate.

--- android-ktx/src/main/java/com/mirego/trikot/kword/android/ktx/AndroidKWord.kt
+++ android-ktx/src/main/java/com/mirego/trikot/kword/android/ktx/AndroidKWord.kt
@@ -1,19 +1,40 @@
 package com.mirego.trikot.kword.android.ktx
 
-import kotlinx.serialization.ImplicitReflectionSerializer
-import kotlinx.serialization.UnstableDefault
+import com.mirego.trikot.kword.I18N
+import com.mirego.trikot.kword.KWord
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.parseMap
-import com.mirego.trikot.kword.KWord
+import java.io.IOException
 
 object AndroidKWord {
-    @UseExperimental(UnstableDefault::class)
-    @ImplicitReflectionSerializer
     fun setCurrentLanguageCode(code: String) {
-        val fileContent =
-            KWord::class.java.getResource("/translations/translation.$code.json")!!
-                .readText()
-        val json = Json.nonstrict.parseMap<String, String>(fileContent)
-        KWord.changeLocaleStrings(json)
+        setCurrentLanguageCodes(KWord, code)
+    }
+
+    fun setCurrentLanguageCode(i18N: I18N, code: String) {
+        setCurrentLanguageCodes(i18N, code)
+    }
+
+    fun setCurrentLanguageCodes(i18N: I18N, vararg codes: String) {
+        val map = mutableMapOf<String, String>()
+        val variant = mutableListOf<String>()
+        codes.forEach {
+            variant.add(it)
+            val variantPath = variant.joinToString(".")
+            try {
+                val fileContent =
+                    KWord::class.java.getResource("/translations/translation.$variantPath.json")!!
+                        .readText()
+                map.putAll(json.parseMap<String, String>(fileContent))
+            } catch (ioException: IOException) {
+            }
+        }
+        i18N.changeLocaleStrings(map)
+    }
+
+    private val json = Json {
+        ignoreUnknownKeys = true
+        isLenient = true
+        serializeSpecialFloatingPointValues = true
     }
 }
--- kword/src/commonMain/kotlin/com/mirego/trikot/kword/TranslationArgumentsParser.kt
+++ kword/src/commonMain/kotlin/com/mirego/trikot/kword/TranslationArgumentsParser.kt
@@ -1,11 +1,11 @@
 package com.mirego.trikot.kword
 
-class TranslationArgumentsParser {
-    fun replaceInTranslation(translation: String, arguments: Array<out Pair<String, String>>): String {
+internal class TranslationArgumentsParser {
+    fun replaceInTranslation(translation: String, arguments: Map<String, String>): String {
         var parsedTranslation: String = translation
 
         for (argument in arguments) {
-            parsedTranslation = parsedTranslation.replace("{{${argument.first}}}", argument.second)
+            parsedTranslation = parsedTranslation.replace("{{${argument.key}}}", argument.value)
         }
 
         return parsedTranslation
