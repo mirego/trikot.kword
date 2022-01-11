bb1e8323f1927708c3623c68599c2a34dd60011b
Jean-Philippe Martin <jpmartin@mirego.com>
2022-01-11 11:18:46 -0500

    Use 4 spaces indent (instead of 2) (#34)
    * Use 4 spaces indent (instead of 2)
    
    * Update kword-plugin/src/main/groovy/com/mirego/kword/KWordEnumGenerate.groovy
    
    Use `repeat` function
    
    Co-authored-by: Marc-Antoine Fortier <mafortier@mirego.com>
    
    Co-authored-by: Marc-Antoine Fortier <mafortier@mirego.com>

--- kword-plugin/src/main/groovy/com/mirego/kword/KWordEnumGenerate.groovy
+++ kword-plugin/src/main/groovy/com/mirego/kword/KWordEnumGenerate.groovy
@@ -47,6 +47,7 @@ class KWordEnumGenerate extends DefaultTask {
 
     private writeFile(ClassName generatedClassName, TypeSpec.Builder enumBuilder) {
         FileSpec.builder(generatedClassName.packageName, generatedClassName.simpleName)
+            .indent(" ".repeat(4))
             .addType(enumBuilder.build())
             .build()
             .writeTo(getGeneratedDir())
